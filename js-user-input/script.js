e352e1f69ab3c409a574368f5d2f6ea3b4224678
Andrew Kalian <andrewkalian2@gmail.com>
2025-08-05 17:28:34 -0700

    add tailwind

--- js-user-input/script.js
+++ js-user-input/script.js
@@ -1,20 +1,21 @@
 let age;
 
 document.getElementById('btn').onclick = function () {
-    age = document.getElementById('number').value;
-    if(age >= 0) {
-        document.getElementById('h').textContent = "Enter your age god damn it"
-    }
-    else if (age < 13) {
-        document.getElementById('h').textContent = "I wonder what life choices you made to get to this site so young" 
-    }
-    else if (age == 16) {
-        document.getElementById('h').textContent = "Ay Im also 16" 
-    }
-    else if (age > 13 && age < 18) {
-        document.getElementById('h').textContent = "I wonder how you discovered this website at " + age
-    }
-    else {
-        document.getElementById('h').textContent = "Hire me. (unless by the time you are seing this I am a founder XD)"
-    }
+
+        age = document.getElementById('number').value;
+        if (age <= 0) {
+            document.getElementById('h').textContent = "Enter your age god damn it"
+        }
+        else if (age < 13) {
+            document.getElementById('h').textContent = "I wonder what life choices you made to get to this site so young"
+        }
+        else if (age == 16) {
+            document.getElementById('h').textContent = "Ay Im also 16"
+        }
+        else if (age > 13 && age < 18) {
+            document.getElementById('h').textContent = "I wonder how you discovered this website at " + age
+        }
+        else {
+            document.getElementById('h').textContent = "Hire me. (unless by the time you are seing this I am a founder XD)"
+        }
 };
